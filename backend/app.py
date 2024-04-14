40c8f9ec3c041c03fcd9d8e8bd6da693b18954ba
raahy91 <155518587+raahy91@users.noreply.github.com>
2024-04-14 11:51:14 -0400

    Secret key

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 from database_operations import *
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from dotenv import load_dotenv
 from flask_cors import CORS
 import pytz
 
@@ -12,8 +11,7 @@ import re
 app = Flask(__name__)
 CORS(app)
 
-load_dotenv()
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.urandom(64)
 
 @app.route('/register', methods=['POST'])
 def register():
