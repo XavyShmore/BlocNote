b47869f25777721ca14c6a30898deca5b30df222
Xavier Hamel <xavier.hamel.5@ulaval.ca>
2024-04-15 21:48:57 -0400

    Populate extented

--- bd_script/populate.sql
+++ bd_script/populate.sql
@@ -12,14 +12,225 @@ VALUES
     ('elodie.pilon.138@rogers.com', 'lkjhasdlkjashdlkjashdlkj', 'Élodie Pilon', 'Je suis une graphiste et webdesigner'),
     ('shrek.shrek@shrek.tv', 'lkjhasdlkjashdlkjashdlkj', 'Shrek', 'Je suis un ogre vert qui vit dans un marais');
 
-INSERT INTO notes (title)
-VALUE ('This will be a greate note');
 CALL create_note('This will be a great note', 1);
 CALL create_note('J\'aime ta grand mère !!!', 3);
 CALL create_note('Pipi, caca, popo: la meilleure des chansons', 3);
+CALL create_note('Je suis le meilleur du monde', 2);
 
-INSERT INTO user_has_access (user_id, note_id)
-VALUES (2,1),
-       (3,1),
-       (4,1),
-       (5,2);
+CALL create_note('Viva las Vegas Lyrics', 4);
+CALL create_note('Hotel California Lyrics', 4);
+CALL create_note('Hey Jude Lyrics', 4);
+CALL create_note('Stairway to Heaven Lyrics', 4);
+CALL create_note('Imagine Lyrics', 4);
+CALL create_note('Billie Jean Lyrics', 4);
+CALL create_note('Sweet Child O Mine Lyrics', 4);
+CALL create_note('Smells Like Teen Spirit Lyrics', 4);
+CALL create_note('Wonderwall Lyrics', 4);
+CALL create_note('Yesterday Lyrics', 4);
+CALL create_note('Like a Rolling Stone Lyrics', 4);
+CALL create_note('Livin on a Prayer Lyrics', 4);
+CALL create_note('Don''t Stop Believin'' Lyrics', 4);
+CALL create_note('Wish You Were Here Lyrics', 4);
+CALL create_note('Piano Man Lyrics', 4);
+
+CALL create_note('Viva las Vegas Guitar Tab', 5);
+CALL create_note('Bohemian Rhapsody Guitar Tab', 5);
+CALL create_note('Hotel California Guitar Tab', 5);
+CALL create_note('Hey Jude Guitar Tab', 5);
+CALL create_note('Stairway to Heaven Guitar Tab', 5);
+CALL create_note('Imagine Guitar Tab', 5);
+CALL create_note('Billie Jean Guitar Tab', 5);
+CALL create_note('Sweet Child O Mine Guitar Tab', 5);
+CALL create_note('Smells Like Teen Spirit Guitar Tab', 5);
+CALL create_note('Wonderwall Guitar Tab', 5);
+CALL create_note('Yesterday Guitar Tab', 5);
+CALL create_note('Like a Rolling Stone Guitar Tab', 5);
+CALL create_note('Livin on a Prayer Guitar Tab', 5);
+CALL create_note('Don''t Stop Believin'' Guitar Tab', 5);
+CALL create_note('Wish You Were Here Guitar Tab', 5);
+CALL create_note('Piano Man Guitar Tab', 5);
+
+CALL create_note('Note du cours 1', 6);
+CALL create_note('Note du cours 2', 6);
+CALL create_note('Note du cours 3', 6);
+CALL create_note('Note du cours 4', 6);
+CALL create_note('Note du cours 5', 6);
+CALL create_note('Note du cours 6', 6);
+CALL create_note('Note du cours 7', 6);
+CALL create_note('Note du cours 8', 6);
+CALL create_note('Note du cours 9', 6);
+CALL create_note('Note du cours 10', 6);
+CALL create_note('Note du cours 11', 6);
+CALL create_note('Note du cours 12', 6);
+CALL create_note('Révision pour l\'examen final', 6);
+
+CALL create_note('Note du cours 1', 6);
+CALL create_note('Note du cours 2', 6);
+CALL create_note('Note du cours 3', 6);
+CALL create_note('Note du cours 4', 6);
+CALL create_note('Note du cours 5', 6);
+CALL create_note('Note du cours 6', 6);
+CALL create_note('Note du cours 7', 6);
+CALL create_note('Note du cours 8', 6);
+CALL create_note('Note du cours 9', 6);
+CALL create_note('Note du cours 10', 6);
+CALL create_note('Note du cours 11', 6);
+CALL create_note('Note du cours 12', 6);
+CALL create_note('Révision pour l\'examen final', 6);
+
+CALL create_note('Le Corbeau', 7);
+CALL create_note('L\'Albatros', 7);
+CALL create_note('Demain, dès l''aube', 7);
+CALL create_note('Invictus', 7);
+CALL create_note('Le Dormeur du val', 7);
+CALL create_note('Demain dès l''aube', 7);
+CALL create_note('Le Lac', 7);
+CALL create_note('Les Fleurs du Mal', 7);
+CALL create_note('Le Bateau ivre', 7);
+CALL create_note('Les Chants de Maldoror', 7);
+CALL create_note('Le Cygne', 7);
+CALL create_note('Le Pont Mirabeau', 7);
+CALL create_note('Ode à la nuit', 7);
+CALL create_note('La Complainte du pauvre Derrida', 7);
+
+CALL create_note('Poulet rôti aux herbes', 8);
+CALL create_note('Lasagnes maison', 8);
+CALL create_note('Tarte aux pommes rustique', 8);
+CALL create_note('Risotto aux champignons', 8);
+CALL create_note('Salade César', 8);
+CALL create_note('Pain fait maison', 8);
+CALL create_note('Pâtes à la carbonara', 8);
+
+CALL create_note('Blague de Toto : À l''école', 9);
+CALL create_note('Blague sur les poules', 9);
+CALL create_note('Blague de Toto : À la pêche', 9);
+CALL create_note('Blague sur les vaches', 9);
+CALL create_note('Blague de Toto : À la plage', 9);
+CALL create_note('Blague sur les blondes', 9);
+CALL create_note('Blague de Toto : À l''épicerie', 9);
+CALL create_note('Blague sur les animaux : Pourquoi les girafes ont-elles un long cou ?', 9);
+CALL create_note('Blague sur la nourriture : Pourquoi les champignons sont-ils de mauvais menteurs ?', 9);
+CALL create_note('Blague sur les métiers : Pourquoi les photographes sont-ils de mauvais magiciens ?', 9);
+CALL create_note('Blague sur la nature : Pourquoi les arbres n''aiment-ils pas les maths ?', 9);
+CALL create_note('Blague sur les objets : Pourquoi les crayons sont-ils de mauvais conducteurs ?', 9);
+CALL create_note('Blague sur la musique : Pourquoi les pianos ne peuvent-ils pas jouer du tennis ?', 9);
+CALL create_note('Blague sur la technologie : Pourquoi les ordinateurs sont-ils de mauvais danseurs ?', 9);
+
+CALL create_note('Citations préférées de films d\'ogres', 11);
+CALL create_note('Recettes d\'ogre', 11);
+CALL create_note('Conseils pour l\'hygiène personnelle', 11);
+CALL create_note('Liste de souhaits pour le marécage', 11);
+CALL create_note('Observations sur les humains', 11);
+CALL create_note('Idées de loisirs', 11);
+CALL create_note('Réflexions sur l\'amitié, Pensées sur l\'importance de l\'amitié, notamment sa relation avec l\'Âne', 11);
+CALL create_note('Rêves et aspirations', 11);
+CALL create_note('Stratégies de défense du marécage, Idées pour protéger son marécage des intrus', 11);
+
+INSERT INTO notebooks (title, owner_id) VALUES
+    ('Chanson à pratiquer', 4),
+    ('Chanson pour guitare', 5),
+    ('Note du cours GLO-2005', 6),
+    ('Note du cours GLO-3500', 6),
+    ('Poème pour Marie', 7),
+    ('Mon recueil de blague de Toto', 9),
+    ('J\'essaye d\'être organisé', 11);
+
+INSERT INTO notebook_contains (note_id, notebook_id) VALUES
+    (5,1),
+    (6,1),
+    (7,1),
+    (8,1),
+    (9,1),
+    (10,1),
+    (11,1),
+    (12,1),
+    (13,1),
+    (14,1),
+    (15,1),
+    (16,1),
+    (17,1),
+    (18,1),
+    (19,1),
+    (20,2),
+    (21,2),
+    (22,2),
+    (23,2),
+    (24,2),
+    (25,2),
+    (26,2),
+    (27,2),
+    (28,2),
+    (29,2),
+    (30,2),
+    (31,2),
+    (32,2),
+    (33,2),
+    (34,2),
+    (35,2),
+    (36,3),
+    (37,3),
+    (38,3),
+    (39,3),
+    (40,3),
+    (41,3),
+    (42,3),
+    (43,3),
+    (44,3),
+    (45,3),
+    (46,3),
+    (47,3),
+    (48,3),
+    (49,4),
+    (50,4),
+    (51,4),
+    (52,4),
+    (53,4),
+    (54,4),
+    (55,4),
+    (56,4),
+    (57,4),
+    (58,4),
+    (59,4),
+    (60,4),
+    (61,4),
+    (62,5),
+    (63,5),
+    (64,5),
+    (65,5),
+    (66,5),
+    (67,5),
+    (68,5),
+    (69,5),
+    (70,5),
+    (83,6),
+    (85,6),
+    (87,6),
+    (89,6),
+    (97,7),
+    (98,7),
+    (99,7),
+    (103,7);
+
+INSERT INTO user_has_access (user_id, note_id) VALUES
+    (2, 62),
+    (2, 63),
+    (2, 64),
+    (2, 65),
+    (2, 66),
+    (2, 67),
+    (2, 68),
+    (2, 69),
+    (2, 70);
+
+INSERT INTO versions (note_id, editor_id, content) VALUES
+    (102, 11, 'Manger Humain'),
+    (102, 11, 'Manger Humain, Jouer au tennis'),
+    (102, 11, 'Manger Humain !!!, Jouer au tennis'),
+    (102, 11, 'Jouer au tennis'),
+    (1, 1, 'haha en effet c\'est la meilleur'),
+    (66, 7, 'C\'est un trou de verdure où chante une rivière'),
+    (66, 2, 'C\'est un trou de verdure où chante une rivière Accrochant follement aux herbes des haillons'),
+    (66, 2, 'C\'est un trou de verdure où chante une rivière Accrochant follement aux herbes des haillons D\'argent ; où le soleil, de la montagne fière,'),
+    (66, 2, 'Mon poème préféré!!!: C\'est un trou de verdure où chante une rivière Accrochant follement aux herbes des haillons D\'argent ; où le soleil, de la montagne fière,');
+
+
